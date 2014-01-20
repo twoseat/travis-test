0302dec82dfd22cc5275228b23e571557cb86e63
Paul Harris <pharris@gopivotal.com>
2014-01-20 15:04:16 +0000

    Adding Tomcat Download and test

--- Gemfile
+++ Gemfile
@@ -3,3 +3,4 @@ source "https://rubygems.org"
 gem 'rake'
 gem 'rspec', '>= 3.0.0beta1'
 gem 'rubocop'
+gem 'rest-client'
--- spec/basic_spec.rb
+++ spec/basic_spec.rb
@@ -1,6 +1,22 @@
-describe 'Basic' do
-  it 'should pass in all cases' do
-    a = 4
-    expect(a).to eq(4)
+require 'rest_client'
+
+describe 'Deploy' do
+
+  let(:tomcat_version) { '7.0.50' }
+
+  before do
+    response = RestClient.get "http://mirror.gopotato.co.uk/apache/tomcat/tomcat-7/v#{tomcat_version}/bin/apache-tomcat-#{tomcat_version}.tar.gz"
+    File.open("/tmp/apache-tomcat-#{tomcat_version}.tar.gz", 'w').write response
+    system "tar zxf /tmp/apache-tomcat-#{tomcat_version}.tar.gz -C /tmp"
   end
+
+  it ' expects a /bin/catalina.sh file ' do
+    expect(File).to exist("/tmp/apache-tomcat-#{tomcat_version}/bin/catalina.sh")
+  end
+
+  after do
+    FileUtils.rm_r "/tmp/apache-tomcat-#{tomcat_version}"
+    FileUtils.rm_r "/tmp/apache-tomcat-#{tomcat_version}.tar.gz"
+  end
+
 end
