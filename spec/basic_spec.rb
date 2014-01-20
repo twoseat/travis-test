6ccb2a11515b5f2156d7dcc7e17c5391ca051a6a
Paul Harris <pharris@gopivotal.com>
2014-01-20 15:39:22 +0000

    Added caching of Tomcat download

--- spec/basic_spec.rb
+++ spec/basic_spec.rb
@@ -5,18 +5,24 @@ describe 'Deploy' do
   let(:tomcat_version) { '7.0.50' }
 
   before do
-    response = RestClient.get "http://mirror.gopotato.co.uk/apache/tomcat/tomcat-7/v#{tomcat_version}/bin/apache-tomcat-#{tomcat_version}.tar.gz"
-    File.open("/tmp/apache-tomcat-#{tomcat_version}.tar.gz", 'w').write response
-    system "tar zxf /tmp/apache-tomcat-#{tomcat_version}.tar.gz -C /tmp"
+    retrieve_tomcat unless File.exist?("vendor/apache-tomcat-#{tomcat_version}.tar.gz")
+  end
+
+  before do
+    system "tar zxf vendor/apache-tomcat-#{tomcat_version}.tar.gz -C vendor"
   end
 
   it ' expects a /bin/catalina.sh file ' do
-    expect(File).to exist("/tmp/apache-tomcat-#{tomcat_version}/bin/catalina.sh")
+    expect(File).to exist("vendor/apache-tomcat-#{tomcat_version}/bin/catalina.sh")
   end
 
   after do
-    FileUtils.rm_r "/tmp/apache-tomcat-#{tomcat_version}"
-    FileUtils.rm_r "/tmp/apache-tomcat-#{tomcat_version}.tar.gz"
+    FileUtils.rm_r "vendor/apache-tomcat-#{tomcat_version}"
+  end
+
+  def retrieve_tomcat
+    response = RestClient.get "http://mirror.gopotato.co.uk/apache/tomcat/tomcat-7/v#{tomcat_version}/bin/apache-tomcat-#{tomcat_version}.tar.gz"
+    File.open("vendor/apache-tomcat-#{tomcat_version}.tar.gz", 'w').write response
   end
 
 end
