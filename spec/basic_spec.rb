f6e481b1e73afe0ef92c4914dedbcd6d0eada977
Paul Harris <pharris@gopivotal.com>
2014-01-21 11:11:04 +0000

    Rubocop

--- spec/basic_spec.rb
+++ spec/basic_spec.rb
@@ -9,7 +9,6 @@ describe 'Deploy' do
   let(:cache_file) { Pathname.new("vendor/apache-tomcat-#{tomcat_version}.tar.gz") }
   let(:tomcat_url) { "http://mirror.gopotato.co.uk/apache/tomcat/tomcat-7/v#{tomcat_version}/bin/apache-tomcat-#{tomcat_version}.tar.gz" }
 
-
   before do
     unless cache_file.exist?
       response = RestClient.get tomcat_url
