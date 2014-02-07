7c372da9ca0d9abc5a39301dfc21b3e6d6f047b8
Paul Harris <pharris@gopivotal.com>
2014-02-07 12:59:42 +0000

    Removed original sample test; fixed Rubocop errors

--- spec/basic_spec.rb
+++ spec/basic_spec.rb
@@ -4,11 +4,6 @@ require 'tomcat_helper'
 describe 'Deploy' do
   include_context 'tomcat_helper'
 
-  it 'expects a bin/catalina.sh file for Tomcat',
-     fixture: 'server' do
-    tomcat_metadatas.each { |tomcat_metadata| expect(tomcat_metadata[:location] + 'bin/catalina.sh').to exist }
-  end
-
   it 'expects session data to be stored and retrieved from a single instance',
      fixture: 'server' do
     tomcat_metadatas.each do |tomcat_metadata|
@@ -16,7 +11,7 @@ describe 'Deploy' do
       location = "http://localhost:#{tomcat_metadata[:http_port]}/session"
       response = RestClient.post location, session_data
       cookies = response.cookies
-      expect(RestClient.get location, {:cookies => cookies}).to eq(session_data + "=")
+      expect(RestClient.get location, cookies: cookies).to eq(session_data + '=')
     end
   end
 end
